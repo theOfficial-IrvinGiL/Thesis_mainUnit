7c6f9f6f0e4dc923469709d76898b3ec951c63f8
Engr. Irvin Gil <84879504+theOfficial-IrvinGiL@users.noreply.github.com>
2022-02-24 19:38:58 +0800

    commit on feb. 24 2022 (#1)

--- Thesis_mainUnit/Main_Unit/Main_Unit.ino
+++ Thesis_mainUnit/Main_Unit/Main_Unit.ino
@@ -25,11 +25,13 @@ Keys:
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH1106.h>
 #include <SPI.h> // for sdcard
+
+// code for sd card component
 #include <SD.h>
 File myFile;
 
 // code for defining the size of the Array
-#define ARRAY_SIZE(x) sizeof(x)/sizeof(x[0])
+#define ARRAY_SIZE(x) sizeof(x) / sizeof(x[0])
 
 #define OLED_RESET 4
 #define SCREEN_ADDRESS 0x3C
@@ -37,298 +39,337 @@ const byte ROWS = 4;
 const byte COLS = 4;
 
 char hexaKeys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 byte rowPins[ROWS] = {2, 3, 4, 5};
 byte colPins[COLS] = {A0, A1, A2, A3};
 
-//object instantiation for the OLED display
+// object instantiation for the OLED display
 Adafruit_SH1106 display(OLED_RESET);
 
 #if (SH1106_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SH1106.h!");
 #endif
 
-//object instantiation for the keypad
+// object instantiation for the keypad
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
-#define relayPin 6 //used for the relay pin
 
-//variable declarations are coded here
-  //for getting administrator passcode
-  char admin_input[8];
-  short admin_fixedInput = 0;
-  
-  char contact_input[11]; keeping 
-  int addressOnEEPROM = 0;
-  short setCursor_column = 0;
-  short fixedNumberOfInputs = 0;
-  char codeFromEEPROM[4];
-  String adminpass [] = {"77351071", "27326699", "82736918", "61835240", "11808191", "12105460","27651616", "65752500", "74198158", "91741377"};
+// variable declarations are coded here
+// for getting administrator passcode
+char admin_input[8];
+short admin_fixedInput = 0;
 
-/** 
-variables used in for the displaying the user 
-input for getting the admin passcode
+unsigned short setCursorCollumn = 0; // used as reference point for the cursor when writting
+
+char contact_input[11];
+char passcode_input[4];
+
+int addressOnEEPROM = 0;
+short fixedNumberOfInputs = 0;
+char codeFromEEPROM[4];
+String predefined_adminpass[] = {"77351071", "27326699", "82736918", "61835240"};
+
+/**
+variables  used for identifying different modes for execution
 */
-short setCursorColumn_adminInput = 0;
+boolean accessGranted = true;
 
+// void showUserDelist() {
+//   display.setTextSize(1);
+//   display.setCursor(0, 2);
+//   display.println("Enter contact number to delist:");
+//   display.setCursor(32, 32);
+//   display.println("XXXXXXXXXXX");
+//   display.display();
+// }
 
+// void showDelistedMsg() {
+//   display.setTextSize(1);
+//   display.setCursor(18, 20);
+//   display.println("Contact Delisted");
+//   display.setCursor(30, 32);
+//   display.println("Successfully");
+//   display.display();
+// }
 
+// void showDelisteErrorMsg() {
+//   display.setTextSize(1);
+//   display.setCursor(2, 12);
+//   display.println("The Contact that you");
+//   display.setCursor(6, 25);
+//   display.println ("have entered is not");
+//   display.setCursor(11, 38);
+//   display.println ("Registered on the");
+//   display.setCursor(45, 51);
+//   display.println ("System.");
+//   display.display();
+// }
 
-void showMainUnit() {
+// method codes for displaying interface into the oled module
+// method to show "Main Unit" message on display
+void showMainUnit()
+{
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(2);
-  display.setCursor(4, 10);
+  display.setCursor(10, 28);
   display.print("Main Unit");
   display.display();
   delay(4000);
   display.clearDisplay();
 }
 
-void sdCARD(String writeThis_toSD) {
-  Serial.begin(9600);
-  while (!Serial) {
-  // wait for  serial port to connect. Needed for native USB port only
-  }
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(10)) {
-    Serial.println("initialization failed!");
-  while (1);
-  }
-  Serial.println("initialization done.");
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println(writeThis_toSD);
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-}
-
-//method for prompting and asking for user input
-void showInputPasscode() {
+// method to show the "input admin passcode" on display
+void showInputPasscode()
+{
+  display.clearDisplay();
   display.setTextSize(1);
-  display.setCursor(0, 0);
-  display.println("Input Administration Passcode: ");
+  display.setCursor(0, 6);
+  display.println("Input Admin Passcode: ");
   display.display();
 }
-
-//show user input on oled
-void showsUser_input(char key) {
-  display.setTextColor(WHITE);
+// method for showing characters from user input
+void displayChar_input(char input, short setcursor_Collumn)
+{
   display.setTextSize(2);
-  display.setCursor(setCursor_column, 15);
-  display.print(key);
-  setCursor_column += 12;
+  display.setCursor(setcursor_Collumn, 30);
+  display.println(input);
+  display.display();
+}
+// method to promp register user function
+
+
+void showRegisterUser()
+{
+  display.setTextSize(1);
+  display.setCursor(0, 2);
+  display.println("Contact Number:");
+  display.setCursor(0, 47);
+  display.println("A - Enter");
+  display.setCursor(0, 57);
+  display.println("B - Clear");
   display.display();
 }
 
-//
-void showMessage(String message) {
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(2);
-  display.setCursor(4, 10);
-  display.print(message);
+// method to show menu to display
+void showMenu()
+{
+  display.setTextSize(1);
+  display.setCursor(0, 8);
+  display.println("         MENU                              A - Register User     B - Delist User      C - View Users List  D - Control Mode");
   display.display();
-  delay(4000);
-  display.clearDisplay();
-}
 
-//method for getting the administrator passcode
-void getAdmin_passcode(){
-  //loop for getting input from keypad
-  while (true){
-      char keyValue = customKeypad.getKey();
-      if (keyValue) {
-        switch (keyValue) {
-          case 'A':
-          
-          break;
-            
-          case 'B':
+  // get user choice
+  char keyValue = customKeypad.getKey();
+  if (keyValue)
+  {
+    switch (keyValue)
+    {
+    // register user
+    case 'A':
+      break;
 
-          break;
+    // delist user
+    case 'B':
+      break;
 
-          case 'C':
-          
-          break;
-          default:
-            if (fixedNumberOfInputs < 11) {
-              user_input[fixedNumberOfInputs] = keyValue;
-              showsUser_input(keyValue);
-              keyValue = 0x00;
-              fixedNumberOfInputs++;
-            }
-            break; 
+    // view user list
+    case 'C':
+      break;
 
-        }
-      }
+    // operate on control mode
+    case 'D':
+      break;
+    }
   }
 }
 
-
-
+// method to show passcode of the new registered user
+void showRegisteredMsg(String new_passcode)
+{
+  display.setTextSize(1);
+  display.setCursor(0, 2);
+  display.println("Your Passcode is:");
+  display.setTextSize(2);
+  display.setCursor(37, 22);
+  display.println(new_passcode);
+  display.setTextSize(1);
+  display.setCursor(0, 49);
+  display.println("You have 20 seconds  to save this code.");
+  display.display();
+}
 
 /** = = = = = = main code starts here = = = = = == */
 
-void setup() {
+void setup()
+{
   Wire.begin();
   Serial.begin(9600);
 
   display.begin(SH1106_SWITCHCAPVCC, 0x3C);
 
   showMainUnit();
-  pinMode(relayPin, OUTPUT);
+  delay(2000);
 }
 
+// int contactNumber [];
+// int fixedContactNumber = 0;
 
-int contactNumber [];
-int fixedContactNumber = 0;
+void loop()
+{
+start_Point:
+  char keyValue = customKeypad.getKey();
+  showInputPasscode();
 
-void loop() {
- start_Point:
+  while (true)
+  {
+    if (accessGranted = true)
+    {
+      char keyValue = customKeypad.getKey();
+      if (keyValue)
+      {
+        switch (keyValue)
+        {
+        case 'A':
+          // loop to look and compare values obtained from user to the predefined admin passcodes
+          for (int x = 0; x < 10; x++)
+          {
+            String str_admin = admin_input;
+            // determine if the user input stored in array is equal to one to the contents in the adminpass array
+            if (str_admin.compareTo(predefined_adminpass[x]) == 1)
+            {
+              showMenu();
+            }
+          }
 
- char keyValue = customKeypad.getKey();
- if (keyValue){
-   switch (keyValue){
-     case 'A':
-     //loop to look and compare values obtained from user to the predefined admin passcodes
-     for(int x = 0; x < 10; x++){
-       //determine if the user input stored in array is equal to one to the contents in the adminpass array
-        if(contact_input.compareTo(adminpass[x]) == 1){
+          break;
 
+        case 'B':
+          break;
+
+        default:
+          // first method for user input entry, number input from user for administrator authentication
+          if (admin_fixedInput < sizeof(admin_input))
+          {
+            admin_input[admin_fixedInput] = keyValue;
+            admin_fixedInput++;
+
+            // display the user input into the oled
+            displayChar_input(keyValue, setCursorCollumn);
+            setCursorCollumn += 12;
+          }
+          break;
         }
-     }
-      
-     break;
-
-     case 'B':
-     break;
-
-     default:
-     //first method for user input entry, number input from user for administrator authentication
-      if (fixedNumberOfInputs < sizeof(contact_input)){
-        contact_input[fixedNumberOfInputs] = keyValue;
-        fixedNumberOfInputs++;
       }
-     break;
-   }
- }
-
-
-
+    }
+    else
+    {
+      break;
+    }
+  }
 }
-  // //goto point
-  // clearScreen:
 
-  // showInputPasscode();
-  // char keyValue = customKeypad.getKey();
-  // if (keyValue) {
-  //   switch (keyValue) {
-  //     case 'A':
-  //         display.setTextColor(WHITE);
-  //         display.setTextSize(2);
-  //         display.setCursor(4, 10);
-  //         display.print("Enter Contact Number: ");
-  //         display.setCursor(50, 10)
-  //         display.print("A. Enter     B. Clear")
-  //         display.setCursor(15, 10)
-  //         display.display();
-  //         delay(4000);
-  //         display.clearDisplay();
-  //         display.display();
-  //         keyValue = 0x00;
+// //goto point
+// clearScreen:
 
-  //             char keyValue = customKeypad.getKey();
-  //               if (keyValue) {
-  //                 switch (keyValue) {
-  //                   case 'A': 
-  //                     display.setTextColor(WHITE);
-  //                     display.setTextSize(2);
-  //                     display.setCursor(4, 10);
-  //                     display.print("Proceed to generate code");
-  //                     display.display();
-  //                     delay(2000);
-  //                     display.clearDisplay();
-  //                     display.display();
-  //                     keyValue = 0x00;
-  //                   case 'B': 
-  //                     display.clearDisplay();
-  //                     setCursor_column = 0;
-  //                     keyValue = 0x00;
-  //                     memset(contact_input, 0, sizeof(contact_input));
-  //                     fixedContactNumber = 0;
-  //                     goto clearScreen;
-  //                   break;
+// showInputPasscode();
+// char keyValue = customKeypad.getKey();
+// if (keyValue) {
+//   switch (keyValue) {
+//     case 'A':
+//         display.setTextColor(WHITE);
+//         display.setTextSize(2);
+//         display.setCursor(4, 10);
+//         display.print("Enter Contact Number: ");
+//         display.setCursor(50, 10)
+//         display.print("A. Enter     B. Clear")
+//         display.setCursor(15, 10)
+//         display.display();
+//         delay(4000);
+//         display.clearDisplay();
+//         display.display();
+//         keyValue = 0x00;
 
-  //                 default:
-  //                   if (fixedContactNumber < 11) {
-  //                   contact_input[fixedContactNumber] = keyValue;
-  //                   showsUser_input(keyValue);
-  //                   keyValue = 0x00;
-  //                   fixedContactNumber++;
-  //                 }
-  //                 break;
-  //       break;
+//             char keyValue = customKeypad.getKey();
+//               if (keyValue) {
+//                 switch (keyValue) {
+//                   case 'A':
+//                     display.setTextColor(WHITE);
+//                     display.setTextSize(2);
+//                     display.setCursor(4, 10);
+//                     display.print("Proceed to generate code");
+//                     display.display();
+//                     delay(2000);
+//                     display.clearDisplay();
+//                     display.display();
+//                     keyValue = 0x00;
+//                   case 'B':
+//                     display.clearDisplay();
+//                     setCursor_column = 0;
+//                     keyValue = 0x00;
+//                     memset(contact_input, 0, sizeof(contact_input));
+//                     fixedContactNumber = 0;
+//                     goto clearScreen;
+//                   break;
 
-  //     case 'B':
-  //         if (fixedNumberOfInputs < 10) {
-  //         display.setTextColor(WHITE);
-  //         display.setTextSize(2);
-  //         display.setCursor(4, 10);
-  //         display.print("Error");
-  //         display.display();
-  //         delay(4000);
-  //         display.clearDisplay();
-  //         display.display();
-  //         keyValue = 0x00;
-  //       }
-  //       else {
-          
-          
-  //         goto clearScreen;
-  //         break;
-  //       default:
-  //         if (fixedNumberOfInputs < 11) {
-  //           user_input[fixedNumberOfInputs] = keyValue;
-  //           showsUser_input(keyValue);
-  //           keyValue = 0x00;
-  //           fixedNumberOfInputs++;
-  //         }
-  //         break;
-        
-  //     case 'C':
-  //           display.setTextColor(WHITE);
-  //           display.setTextSize(2);
-  //           display.setCursor(4, 10);
-  //           display.display();
-            
-  //           for(int x = 0; x <(EEPROM.read(0)); x++){
-  //           String retrievedString = readStringFromEEPROM(eepromAddress);
-  //           if retrievedString.length() == 11{
-  //               display.print(retrievedString)
-  //           }
-            
-  //           delay(4000);
-  //           display.clearDisplay();
-  //           display.display();
-  //           keyValue = 0x00;
+//                 default:
+//                   if (fixedContactNumber < 11) {
+//                   contact_input[fixedContactNumber] = keyValue;
+//                   showsUser_input(keyValue);
+//                   keyValue = 0x00;
+//                   fixedContactNumber++;
+//                 }
+//                 break;
+//       break;
+
+//     case 'B':
+//         if (fixedNumberOfInputs < 10) {
+//         display.setTextColor(WHITE);
+//         display.setTextSize(2);
+//         display.setCursor(4, 10);
+//         display.print("Error");
+//         display.display();
+//         delay(4000);
+//         display.clearDisplay();
+//         display.display();
+//         keyValue = 0x00;
+//       }
+//       else {
+
+//         goto clearScreen;
+//         break;
+//       default:
+//         if (fixedNumberOfInputs < 11) {
+//           user_input[fixedNumberOfInputs] = keyValue;
+//           showsUser_input(keyValue);
+//           keyValue = 0x00;
+//           fixedNumberOfInputs++;
+//         }
+//         break;
+
+//     case 'C':
+//           display.setTextColor(WHITE);
+//           display.setTextSize(2);
+//           display.setCursor(4, 10);
+//           display.display();
+
+//           for(int x = 0; x <(EEPROM.read(0)); x++){
+//           String retrievedString = readStringFromEEPROM(eepromAddress);
+//           if retrievedString.length() == 11{
+//               display.print(retrievedString)
+//           }
+
+//           delay(4000);
+//           display.clearDisplay();
+//           display.display();
+//           keyValue = 0x00;
 
 // ---------------------------------------------E-N-D--------------------------------------
-  // display.clearDisplay();
-  //       setCursor_column = 0;
-  //       keyValue = 0x00;
-  //       memset(user_input, 0, sizeof(user_input));
-  //       fixedNumberOfInputs = 0;
-  //       goto clearScreen;
+// display.clearDisplay();
+//       setCursor_column = 0;
+//       keyValue = 0x00;
+//       memset(user_input, 0, sizeof(user_input));
+//       fixedNumberOfInputs = 0;
+//       goto clearScreen;
